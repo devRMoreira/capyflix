82ddd044e95fece7d4704c98d072052d2c3240d7
RM <devRMoreira@gmail.com>
2024-02-29 19:32:30 +0000

    Adicionado metodo PATCH

--- src/pages/api/utilizador/[id].js
+++ src/pages/api/utilizador/[id].js
@@ -21,6 +21,21 @@ export default async function handler(req, res) {
             return res.status(403).json(utilizador)
         }
 
+    } else if (req.method === "PATCH") {
+
+        const quemSeguir = req.query.id
+        const novoSeguidor = req.body.id
+
+        if (quemSeguir.length !== 24) {
+            return res.status(403).json({
+                mensagem: "ID inválido."
+            })
+        }
+
+        return res.status(200).json(undefined)
+
+
+
     } else {
         return res.status(404).json(undefined)
     }
