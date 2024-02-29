0b91fcbdec4ad1d1d003eedd1f306552d66bb438
RM <devRMoreira@gmail.com>
2024-02-29 18:52:32 +0000

    criado data/estatisticas.js

---
+++ src/backend/data/estatisticas.js
@@ -0,0 +1,11 @@
+// const timer = setTimeout(() => {
+//     someFunction()
+// }, 1000);
+
+// async function atualizarMediaAvaliacoes() {
+
+// }
+
+//async function atualizarVisualizacoes() {
+
+//}
