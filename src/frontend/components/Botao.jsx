21dc2a0eb8127b5c98d899d372b531dda3d12cd4
EduardoSilva93 <earsilva0218@gmail.com>
2024-03-01 12:16:40 +0000

    Alterações

--- src/frontend/components/Botao.jsx
+++
@@ -1,7 +0,0 @@
-export function Botao({ title, ...rest }) {
-  return (
-    <button className=" font-semibold px-8 py-2 border-4 border-main-orange rounded-lg bg-fundo-botao text-main-white">
-      {title}
-    </button>
-  );
-}
--- src/frontend/components/botao.jsx
+++
@@ -1,9 +0,0 @@
-
-
-export function Botao({ title, ...rest }) {
-  return (
-    <button className=" font-semibold px-8 py-2 border-4 border-laranja-principal rounded-lg bg-fundo-botao text-white">
-      {title}
-    </button>
-  );
-}
