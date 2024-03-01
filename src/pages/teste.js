d8b9c2212d6fb9db04f679065cbf194c1bc07959
EduardoSilva93 <earsilva0218@gmail.com>
2024-03-01 12:18:48 +0000

    Teste

---
+++ src/pages/teste.js
@@ -0,0 +1,14 @@
+import { Inter } from "next/font/google";
+import Recomendacoes from "@/frontend/components/Recomendacoes";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <main>
+      <div className=" flex">
+        
+      </div>
+    </main>
+  );
+}
