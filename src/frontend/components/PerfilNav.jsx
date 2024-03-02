9ee8cebe01bf6cced35b009444e484d43bfa9853
Carolina Limeres <carolinalimeres@outlook.com>
2024-03-02 17:14:43 +0000

    navbar pagina perfil

--- src/frontend/components/PerfilNav.jsx
+++ src/frontend/components/PerfilNav.jsx
@@ -1,33 +1,33 @@
 export function PerfilNav({ avatar, username, comentarios, ligacoes, config }) {
   return (
-    <div className=" flex w-full h-20 bg-navbar px-20">
+    <div className=" flex w-full h-20 bg-navbar px-5">
       <div className=" w-full flex justify-between">
         <div className=" flex items-center">
-          <div>
+          <a href="/">
             <img src={avatar} alt="Avatar do Usuário" className=" pr-2"></img>
-          </div>
+          </a>
           <div className=" font-semibold text-laranja-principal">
             {username}
           </div>
         </div>
         <div className=" flex items-center">
-          <div>
+          <a href="/">
             <img
               src={comentarios}
               alt="Comentários do Usuário"
               className="pr-2"
             ></img>
-          </div>
-          <div>
+          </a>
+          <a href="/">
             <img
               src={ligacoes}
               alt="Perfis que o usuário segue e seus seguidores"
               className="pr-2"
             ></img>
-          </div>
-          <div>
+          </a>
+          <a href="/">
             <img src={config} alt="Configurações" className=""></img>
-          </div>
+          </a>
         </div>
       </div>
     </div>
