b1814a1e2ddd03577b8969b4dd676724ad22a165
Carolina Limeres <carolinalimeres@outlook.com>
2024-03-04 15:35:36 +0000

    pagina configuracoes

--- src/pages/configuracoes.js
+++ src/pages/configuracoes.js
@@ -1,9 +1,61 @@
+import { Botao } from "@/frontend/components/botao";
+import { useState } from "react";
+
 export default function Configuracoes() {
+  const [istToggled, setIsToggled] = useState(false);
+
+  const toggleImage = () => {
+    setIsToggled(!istToggled);
+  };
+
   return (
-    <div className="flex flex-col items-center max-w-96 min-h-screen h-full bg-fundo-principal">
+    <div className="flex flex-col max-w-96 min-h-screen h-full bg-fundo-principal">
       <a href="/">
-        <img src="/icones/Back.png"></img>
+        <img src="/icones/Back.png" className=" ml-4 mt-6"></img>
       </a>
+      <button
+        onClick={toggleImage}
+        className=" flex justify-between  mt-12 mx-6 text-main-white border-b border-borda-cinza"
+      >
+        Perfil Privado
+        {istToggled ? (
+          <img src="/icones/toggle-on.png" className=" mb-3"></img>
+        ) : (
+          <img src="/icones/toggle-off.png" className=" mb-3"></img>
+        )}
+      </button>
+      <a
+        href="/"
+        className=" flex justify-between items-center mt-10 mx-6 text-main-white border-b border-borda-cinza"
+      >
+        Alterar Foto de Perfil
+        <img src="/icones/avatar.png" className=" mb-6"></img>
+      </a>
+      <a
+        href="/"
+        className=" flex justify-between items-center mt-10 mx-6 text-main-white border-b border-borda-cinza"
+      >
+        Alterar senha
+        <img src="/icones/key.png" className=" w-7 h-7 mb-6"></img>
+      </a>
+      <a
+        href="/"
+        className=" flex justify-between items-center mt-10 mx-6 text-main-white border-b border-borda-cinza"
+      >
+        Termos de Utilização
+        <img src="/icones/termos.png" className="mb-6"></img>
+      </a>
+      <a
+        href="/"
+        className=" flex justify-between items-center mt-10 mx-6 text-main-white "
+      >
+        Privacidade
+        <img src="/icones/privacidade.png" className="mb-3"></img>
+      </a>
+
+      <div className=" flex justify-center mt-20 mb-24">
+        <Botao title="LOGOUT"></Botao>
+      </div>
     </div>
   );
 }
