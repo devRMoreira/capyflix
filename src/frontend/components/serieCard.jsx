294d73c0ac46a417f3b4ca6c828b102f0e1e8215
EduardoSilva93 <earsilva0218@gmail.com>
2024-03-04 14:43:56 +0000

    Alterações.

--- src/frontend/components/serieCard.jsx
+++ src/frontend/components/serieCard.jsx
@@ -1,3 +1,4 @@
+import moment from "moment";
 import Image from "next/image";
 
 
@@ -7,7 +8,8 @@ export function SerieCard({serie}){
             <Image src={serie.capa} width="100" height="100" />
             <h2>{serie.titulo}</h2>
             <h2>{serie.tituloOriginal}</h2>
-            <h3>{serie.genero.map((ele,index) => index < serie.genero.length-1 ? ele + "/" : ele)}</h3>
+            <h3>{serie.genero.map((ele, index) => index < serie.genero.length - 1 ? ele + "/" : ele)}</h3>
+            <h3 className="text-main-white">Data de Lançamento: {moment(serie.dataLancamento).format('DD/MM/YYYY')}</h3>
             <p>{Object.keys(serie.temporadas).length}</p>
             <p>{serie.classificacaoEtaria}</p>
             <p>{serie.mediaAvaliacoes}</p>
