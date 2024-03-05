4d5166050a579786952dd78752532ae7f55020e1
EduardoSilva93 <earsilva0218@gmail.com>
2024-03-05 14:40:45 +0000

    Alterações.

--- src/frontend/components/serieCompleta.jsx
+++ src/frontend/components/serieCompleta.jsx
@@ -1,30 +1,25 @@
 import moment from "moment";
 import { EpisodioSerie } from "./episodioSerie";
+import { SerieCard } from "./serieCard";
 
 export function SerieCompleta({ serie }) {
     return (
         <div>
-            <div><img src={serie.capa} width="100" height="100" /></div>
-            <h2>{serie.titulo}</h2>
-            <h2>{serie.tituloOriginal}</h2>
-            <h3>{serie.genero}</h3>
-            <h3 className="text-main-white">Data de Lançamento: {moment(serie.dataLancamento).format('DD/MM/YYYY')}</h3>
-            {serie.temporadas.map((ele, i) => <p> Temporada {i + 1}</p>)}
-
-            {serie.temporadas.map((ele, i) => ele.map((ele, i) => <EpisodioSerie numero={i + 1} titulo={ele.titulo} descricao={ele.descricao} />))}
-            <p>{serie.classificacaoEtaria}</p>
-            <p>{serie.mediaAvaliacoes}</p>
+            <div>
+                <SerieCard serie={ serie } />
+            </div>
             <div>
                 <h4>Sinopse</h4>
                 <p>{serie.sinopse}</p>
-                <h4>Elenco</h4>
+                <h4>Elenco:</h4>
                 <p>{serie.elenco.nome}</p>
-                <h4>Realizador</h4>
+                <h4>Realizador:</h4>
                 <p>{serie.realizador.nome}</p>
-                {/*  <TemporadaSeries/>
-                <TemporadaSeries/>
-                <TemporadaSeries/>
-                <TemporadaSeries/> */}
+                <div>
+                    {serie.temporadas.map((ele, i) => <p> Temporada {i + 1}</p>)}
+
+                    {serie.temporadas.map((ele, i) => ele.map((ele, i) => <EpisodioSerie numero={i + 1} titulo={ele.titulo} descricao={ele.descricao} />))}
+                </div>
             </div>
         </div>
     )
