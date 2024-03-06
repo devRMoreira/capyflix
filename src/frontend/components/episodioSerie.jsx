b1aee21418e0f76f613206b2b4cb56ae4d4e3315
EduardoSilva93 <earsilva0218@gmail.com>
2024-03-06 14:18:41 +0000

    Alteração.

--- src/frontend/components/episodioSerie.jsx
+++ src/frontend/components/episodioSerie.jsx
@@ -1,6 +1,6 @@
 export function EpisodioSerie({ numero, titulo, descricao }) {
   return (
-    <div className="text-main-white text-xs border border-laranja-principal rounded-xl w-48">
+    <div className="text-main-white text-xs border border-laranja-principal rounded-xl w-48 mx-1 my-2">
       <p className="mb-1">Episódio: {numero} - {titulo} </p>
       <p className="text-xxs">{descricao}</p>
     </div>
