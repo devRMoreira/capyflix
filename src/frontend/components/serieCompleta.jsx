79cdab06979526604a3810afda7cb8b763f74459
EduardoSilva93 <earsilva0218@gmail.com>
2024-03-06 12:04:32 +0000

    Modificação de componente serieCompleta.

--- src/frontend/components/serieCompleta.jsx
+++ src/frontend/components/serieCompleta.jsx
@@ -3,8 +3,17 @@ import { SerieCard } from "./serieCard";
 
 export function SerieCompleta({ serie }) {
   return (
-    <div>
-      <SerieCard serie={serie} />
+    <div className="md:h-screen md:w-screen bg-fundo-principal text-main-white relative">
+       <div>
+            <Image src={serie.capa} width="100" height="100" />
+            <h2>{serie.titulo}</h2>
+            <h2>{serie.tituloOriginal}</h2>
+            <h3>{serie.genero.map((ele, index) => index < serie.genero.length - 1 ? ele + "/" : ele)}</h3>
+            <h3 className="text-main-black">Data de Lançamento: {moment(serie.dataLancamento).format('DD/MM/YYYY')}</h3>
+            <p>{Object.keys(serie.temporadas).length}</p>
+            <p>{serie.classificacaoEtaria}</p>
+            <p>{serie.mediaAvaliacoes}</p>
+        </div>
 
       <div>
         <h4>Sinopse</h4>
