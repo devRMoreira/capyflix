2ee8c617876b5f32dfa3d7d70195c3cf46f48c0c
EduardoSilva93 <earsilva0218@gmail.com>
2024-03-06 14:17:13 +0000

    Estilização episodioSerie.

--- src/frontend/components/episodioSerie.jsx
+++ src/frontend/components/episodioSerie.jsx
@@ -1,8 +1,8 @@
 export function EpisodioSerie({ numero, titulo, descricao }) {
   return (
-    <div className="bg-fundo-principal text-main-white text-xs justify-normal border border-laranja-principal rounded-xl w-48 ">
-      <p className="text-sm mb-1">Episódio: {numero} - {titulo} </p>
-      <p className="">{descricao}</p>
+    <div className="text-main-white text-xs border border-laranja-principal rounded-xl w-48">
+      <p className="mb-1">Episódio: {numero} - {titulo} </p>
+      <p className="text-xxs">{descricao}</p>
     </div>
   );
 }
