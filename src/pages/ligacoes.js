aac669993db1f156c1788b4ffd5be1bea6db64e9
Carolina Limeres <carolinalimeres@outlook.com>
2024-03-06 22:10:58 +0000

    adicionado fetch para endpoint de usuário para obter seguidores e seguindo de um usuário (página de ligações)

--- src/pages/ligacoes.js
+++ src/pages/ligacoes.js
@@ -1,8 +1,11 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
+const id = "65e5a01cde0f619624348e79";
 
 export default function Ligacoes() {
   const [followingIsClicked, setFollowingIsClicked] = useState(false);
   const [followersIsClicked, setFollowersIsClicked] = useState(false);
+  const [data, setData] = useState(null);
 
   const dropdownFollowing = () => {
     setFollowingIsClicked(!followingIsClicked);
@@ -12,55 +15,95 @@ export default function Ligacoes() {
     setFollowersIsClicked(!followersIsClicked);
   };
 
+  useEffect(() => {
+    const fetchDataUser = async () => {
+      try {
+        const response = await fetch(
+          `http://localhost:3000/api/utilizador/${id}`
+          // { body: { lista: "visto" } }
+        );
+        if (!response.ok) {
+          throw new Error("Erro ao buscar os dados");
+        }
+        const jsonData = await response.json();
+        await setData(jsonData.utilizadorFiltrado);
+      } catch (error) {
+        console.error("Erro ao buscar os dados:", error);
+      }
+    };
+
+    fetchDataUser();
+  }, []);
+
   return (
-    <div className="flex flex-col md:max-w-96 min-h-screen h-full bg-fundo-principal">
-      <a href="/perfil">
-        <img src="/icones/Back.png" className=" ml-4 mt-10"></img>
-      </a>
-      <div className=" flex gap-16 ml-4 mr-4">
-        <h1 className=" mb-6 text-lg mt-10 font-semibold text-main-white">
-          Seguindo
-        </h1>
-        <button onClick={dropdownFollowing}>
-          {followingIsClicked ? (
-            <img src="/icones/arrowUp.png" className=" mt-4"></img>
-          ) : (
-            <img src="/icones/dropdown.png" className=" mt-4"></img>
-          )}
-        </button>
-      </div>
-      {followingIsClicked && (
-        <>
-          <div className=" flex items-center gap-2 ml-6 mt-2">
+    <>
+      <div className="flex flex-col md:max-w-96 min-h-screen h-full bg-fundo-principal">
+        <a href="/perfil">
+          <img src="/icones/Back.png" className=" ml-4 mt-10"></img>
+        </a>
+        <div className=" flex gap-16 ml-4 mr-4">
+          <h1 className=" mb-6 text-lg mt-10 font-semibold text-main-white">
+            Seguindo
+          </h1>
+          <button onClick={dropdownFollowing}>
+            {followingIsClicked ? (
+              <img src="/icones/arrowUp.png" className=" mt-4"></img>
+            ) : (
+              <img src="/icones/dropdown.png" className=" mt-4"></img>
+            )}
+          </button>
+        </div>
+        {followingIsClicked &&
+          data &&
+          data.quemSegue &&
+          data.quemSegue.length > 0 && (
+            <>
+              {/* <div className=" flex items-center gap-2 ml-6 mt-2">
             <img src="/icones/avatar.png"></img>
             <p className=" text-laranja-principal font-medium">Carolina</p>
-          </div>
-          <div className=" flex items-center gap-2 ml-6 mt-7">
+          </div> */}
+              {data.quemSegue.map((item, index) => (
+                <div key={index} className=" flex items-center gap-2 ml-6 mt-2">
+                  <img src={item?.imagemPerfil}></img>
+                  <p className=" text-laranja-principal font-medium">
+                    {item?.nome}
+                  </p>
+                </div>
+              ))}
+              {/* <div className=" flex items-center gap-2 ml-6 mt-7">
             <img src="/icones/avatar.png"></img>
             <p className=" text-laranja-principal font-medium">João</p>
-          </div>
-        </>
-      )}
-      <div className=" flex gap-12 ml-4 mr-4">
-        <h1 className=" mb-6 text-lg mt-10 font-semibold text-main-white">
-          Seguidores
-        </h1>
-        <button onClick={dropdownFollowers}>
-          {followersIsClicked ? (
-            <img src="/icones/arrowUp.png" className=" mt-4"></img>
-          ) : (
-            <img src="/icones/dropdown.png" className=" mt-4"></img>
+          </div> */}
+            </>
           )}
-        </button>
+        <div className=" flex gap-12 ml-4 mr-4">
+          <h1 className=" mb-6 text-lg mt-10 font-semibold text-main-white">
+            Seguidores
+          </h1>
+          <button onClick={dropdownFollowers}>
+            {followersIsClicked ? (
+              <img src="/icones/arrowUp.png" className=" mt-4"></img>
+            ) : (
+              <img src="/icones/dropdown.png" className=" mt-4"></img>
+            )}
+          </button>
+        </div>
+        {followersIsClicked &&
+          data &&
+          data.seguidores &&
+          data.seguidores.length > 0 && (
+            <>
+              {data.seguidores.map((item, index) => (
+                <div key={index} className=" flex items-center gap-2 ml-6 mt-2">
+                  <img src={item?.imagemPerfil}></img>
+                  <p className=" text-laranja-principal font-medium">
+                    {item?.nome}
+                  </p>
+                </div>
+              ))}
+            </>
+          )}
       </div>
-      {followersIsClicked && (
-        <>
-          <div className=" flex items-center gap-2 ml-6 mt-2 mb-24">
-            <img src="/icones/avatar.png"></img>
-            <p className=" text-laranja-principal font-medium">Ana</p>
-          </div>
-        </>
-      )}
-    </div>
+    </>
   );
 }
