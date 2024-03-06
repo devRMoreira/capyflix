4d73706052b98aa742029b87f45eb192640a9298
RM <devRMoreira@gmail.com>
2024-03-06 15:13:13 +0000

    Fix pesquisa

--- src/frontend/components/cartaoPesquisa.jsx
+++ src/frontend/components/cartaoPesquisa.jsx
@@ -2,16 +2,16 @@ import Image from "next/image";
 import Link from "next/link";
 
 export function CartãoPesquisa({ conteudo }) {
+    
     return (
-        < div className="border border-laranja-principal rounded-xl mt-3 mx-1 h-13 items-center h-10 flex flex-col" >
-            {console.log(conteudo)}
-            <Link href={ `/filme/${conteudo._id}`}>
+        <Link href={conteudo.tipo === "filme" ? "/filme" : "/serie"} onClick={() => handleClick()}>
+            < div className="border border-laranja-principal rounded-xl mt-3 mx-1 h-13 items-center h-10 flex flex-col" >
                 <Image src={conteudo.capa} alt={conteudo.titulo} width="50" height="50" className="ml-2" />
                 <div className="flex flex-col">
                     <p className="text-main-white font-semibold text-xs ml-2">{conteudo.titulo}</p>
                     <p className="text-main-white font-semibold text-xxs italic opacity-50 ml-2">({conteudo.tipo})</p>
                 </div>
-            </Link>
-        </div >
+            </div >
+        </Link>
     )
 }
