4ba7559598ba6d2a3f14904df895acaf62b033d9
RM <devRMoreira@gmail.com>
2024-03-07 09:36:33 +0000

    Alterada página login para o index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,67 @@
-import { Inter } from "next/font/google";
+import { Botao } from "@/frontend/components/botao";
+import { Input } from "@/frontend/components/Input";
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
 
+export default function Login() {
+  const [nome, setNome] = useState("");
+  const [senha, setSenha] = useState("");
 
-const inter = Inter({ subsets: ["latin"] });
+  const handleChangeNome = (event) => {
+    setNome(event.target.value);
+  };
 
-export default function Home() {
+  const handleChangeSenha = (event) => {
+    setSenha(event.target.value);
+  };
+
+  function handleClick(e) {
+    e.preventDefault();
+
+    console.log("Nome:", nome);
+    console.log("Senha:", senha);
+  }
+
   return (
-    <main>
-      <div className=" flex">
-        <h1 className=" bg-main-orange"></h1>
-        <div>TESTE do index</div>
+    <div className=" min-h-screen max-w-96 h-full bg-fundo-principal flex flex-col items-center ">
+      <Image className="mb-8" src="/logo.png" width="300" height="50" />
+
+      <div className="mb-16">
+        <p className=" text-laranja-principal">A Capyvara sabe o que é bom.</p>
       </div>
-    </main>
+      <form onSubmit={handleClick} className=" flex flex-col items-center">
+        <div className="mb-6">
+          <Input
+            icone="/icones/User.png"
+            placeholder="Insere o teu nome de utilizador"
+            type="text"
+            name="username"
+            id="username"
+            value={nome}
+            onChange={handleChangeNome}
+          ></Input>
+        </div>
+        <div className=" mb-20">
+          <Input
+            icone="/icones/Password Key.png"
+            placeholder="Insere a tua palavra-passe"
+            type="password"
+            name="password"
+            id="password"
+            value={senha}
+            onChange={handleChangeSenha}
+          ></Input>
+        </div>
+        <div className=" flex space-x-12 mb-8">
+          <Botao title="LOGIN" />
+        </div>
+        <div className=" mb-10">
+          <Link href="/registar">
+            <Botao title="REGISTAR" />
+          </Link>
+        </div>
+      </form>
+    </div>
   );
 }
