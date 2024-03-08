492607da1ca048e77c59729c331f9efeaeec4d5e
EduardoSilva93 <earsilva0218@gmail.com>
2024-03-08 09:45:09 +0000

    Criação Pagina Elenco

---
+++ src/pages/elenco.js
@@ -0,0 +1,21 @@
+import Link from "next/link";
+
+
+export default function elenco() {
+    return (
+        <div>
+            <div>
+                <img src=""></img>
+            </div>
+            <div>
+                <h2>Nome:</h2>
+                <h3>Data de Nascimento:</h3>
+                <p>Descrição:</p>
+            </div>
+            <div>
+                <p>Participou em:</p>
+                <img src=""></img>
+            </div>
+        </div>
+    )
+}
