f28018d37b4e2dd0923f3f407cbceb0fa5510c7b
EduardoSilva93 <earsilva0218@gmail.com>
2024-03-08 10:54:19 +0000

    Alterações e estilização de página de elenco.

--- src/pages/elenco.js
+++ src/pages/elenco.js
@@ -3,18 +3,26 @@ import Link from "next/link";
 export default function elenco() {
   return (
     <div className=" min-h-screen md:max-w-96 h-full bg-fundo-principal flex flex-col">
-      <div>
-        <img src=""></img>
-      </div>
-      <div>
-        <h2 className=" text-main-white">Nome:</h2>
-        <h3 className=" text-main-white">Data de Nascimento:</h3>
-        <p className=" text-main-white">Descrição:</p>
-      </div>
-      <div>
-        <p>Participou em:</p>
-        <img src=""></img>
-      </div>
+        <Link href="/perfil/user">
+        <img src="/icones/Back.png" className=" ml-4 mt-6"></img>
+        </Link>
+          <div className="flex mt-5 ml-5">
+              <div className="flex justify-center">
+                 <div>
+                    <img src="" className="w-40 h-60"></img>
+                </div>
+                <div className="ml-5">
+                    <h2 className=" text-main-white text-sm font-semibold">Nome </h2>
+                    <h3 className=" text-main-white text-xs mt-2">Data de Nascimento:</h3>
+                    <p className=" text-main-white text-xs mt-2">Descrição:</p>
+                </div>
+          </div>
+          </div>
+          
+        <div className="ml-5 mt-4">
+            <p className="text-main-white text-sm font-semibold">Participou em:</p>
+            <img src="" className="w-20 h-30"></img>
+         </div>
     </div>
   );
 }
